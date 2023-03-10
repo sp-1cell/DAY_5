52998d2fe69741badf4753d4f7224c748d4ab152
sp-1cell <sushant6247@gmail.com>
2023-03-10 20:07:33 +0530

    ADDED EVEN ODD PROBLEM

---
+++ EvenOdd.java
@@ -0,0 +1,17 @@
+package DAY5;
+
+import java.util.Scanner;
+
+class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number to check:");
+        int num = sc.nextInt();
+        if (num % 2 == 0) {
+            System.out.println("even number" + num);
+        } else {
+            System.out.println("odd number" + num);
+        }
+    }
+}
+
